2695bdb3e3ec47add6de977f037218f59e806d30
Mr Centimetre <74942413+mrcentimetre@users.noreply.github.com>
2023-12-06 09:07:24 +0000

    Last Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Udana Nimsara",
+  title: "Undergraduate @ University of Moratuwa & UI/UX Designer",
+  email: "udananimsara@gmail.com",
+  gitHub: "mrcentimetre",
+  instagram: "nimsaraonline",
+  linkedIn: "nimsaraonline",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "nimsaraonline",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
